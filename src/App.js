89a6786e10e38217ba596755da42f265f2dd7d31
Aman Sharma <amansharma4316@gmail.com>
2023-10-18 15:46:58 +0530

    Fixed Bugs

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ function App() {
 
             <UTCDatetime />
             <Link
-              href="https://github.com/Amin-Awinti"
+              href="https://github.com/amansharma4316"
               target="_blank"
               underline="none"
               sx={{ display: 'flex' }}
